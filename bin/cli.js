888f7af1110435b35dbb76ec6b791bb47a2c7f69
Youky1 <youkyf@qq.com>
2022-04-18 10:27:04 +0800

    feat: 更新下载方式与交互

--- bin/cli.js
+++ bin/cli.js
@@ -1,26 +1,36 @@
 #!/usr/bin/env node
-
-const inquirer = require("inquirer");
-const { download } = require("../lib");
-const args = require("minimist")(process.argv.slice(2));
-if (args.default || args.d) {
-  download({
-    downloadRange: "byMid",
-    downloadType: "mp4",
-    content: "313580179",
-  });
-} else {
-  inquirer
-    .prompt([
+(async () => {
+  const inquirer = require("inquirer");
+  const { download } = require("../lib");
+  const args = require("minimist")(process.argv.slice(2));
+  if (args.default || args.d) {
+    download({
+      downloadRange: "byAuthor",
+      downloadType: "mp4",
+      downloadPath: "https://space.bilibili.com/313580179",
+    });
+  } else {
+    const { downloadRange } = await inquirer.prompt([
       {
         type: "list",
         name: "downloadRange",
         message: "要使用的下载方式",
         choices: [
-          { name: "使用【作者mid】下载所有作品", value: "byMid" },
-          { name: "使用【URL】下载单个作品", value: "byUrl" },
+          { name: "使用【UP主页URL】下载所有作品", value: "byAuthor" },
+          { name: "使用【作品URL】下载单个作品", value: "byVideo" },
         ],
       },
+    ]);
+    const { downloadPath } = await inquirer.prompt([
+      {
+        name: "downloadPath",
+        message: `请输入${
+          downloadRange === "byAuthor" ? "【UP主页URL】" : "【视频URL】"
+        }`,
+        validate: (input) => (input ? true : "输入不能为空"),
+      },
+    ]);
+    const { downloadType } = await inquirer.prompt([
       {
         type: "list",
         name: "downloadType",
@@ -30,11 +40,7 @@ if (args.default || args.d) {
           { name: "仅音频", value: "mp3" },
         ],
       },
-      {
-        name: "content",
-        message: "请输入【视频URL】 或 【UP主mid】",
-        validate: (input) => (input ? true : "输入不能为空"),
-      },
-    ])
-    .then((args) => download(args));
-}
+    ]);
+    download({ downloadRange, downloadPath, downloadType });
+  }
+})();
